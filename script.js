1b135b7936ec21db9c633e4bd8a275d9ec9d0080
Sasukeeeeeee07 <156161945+Sasukeeeeeee07@users.noreply.github.com>
2024-06-25 12:22:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const passBox = document.getElementById("password");
+const length = 12;
+const chir = document.getElementById("btn");
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefhgijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*(){}[]/<>|~";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passBox.value = password;
+}
+
+function copyPass()
+{
+  passBox.select();
+  document.execCommand("copy");
+}
